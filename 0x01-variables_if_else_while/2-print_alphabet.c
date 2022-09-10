fcd0b956b8b010b11386be2ee50d99c02f0960f9
Abegunde99 <preciousolanrewaju1998@gmail.com>
2022-09-10 00:38:50 +0000

    I created the file 2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: A program to print alphabets in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+		putchar('\n');
+	}
+
+	return (0);
+}
