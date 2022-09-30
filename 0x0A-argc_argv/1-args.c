11fb2cb51214b8ab266fccf4dcc0be59bdadf073
Abegunde99 <preciousolanrewaju1998@gmail.com>
2022-09-30 20:07:44 +0000

    I created the 1-args.c file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - funtion to print the number of arguements passed into the cl
+* @argc: the number of strings/elements passed
+* @argv: the array passed
+*
+* Return: always return 0
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
