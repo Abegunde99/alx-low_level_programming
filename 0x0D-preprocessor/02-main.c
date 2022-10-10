7b34ba0c40344a11dcb65ad9e03e29a42ca8c7bf
Abegunde99 <preciousolanrewaju1998@gmail.com>
2022-10-10 21:59:26 +0000

    I created the file 2-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - A function to print the name of the current file
+*
+* Return: always return 0
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
